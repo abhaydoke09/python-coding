4398c52eeaeb77143e959eb42d57e2c044361a15
Abhay Doke <abhaydoke09@gmail.com>
2016-11-15 22:24:00 -0500

    Converting an Integer to a String in Any Base

---
+++ integer_to_string_recursion.py
@@ -0,0 +1,10 @@
+n = 2000
+
+def convertToString(n, base):
+	convertString = "0123456789ABCDEF"
+	if n<base:
+		return convertString[n]
+	else:
+		return convertToString(n//base,base) + convertString[n%base]
+
+print convertToString(n,16)
