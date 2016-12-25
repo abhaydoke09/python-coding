1156f4c640c8b5958c1c548bdd0a2762d78a3979
Abhay Doke <abhaydoke09@gmail.com>
2016-12-25 17:07:33 -0500

    leetcode started

--- integer_to_string_recursion.py
+++ integer_to_string_recursion.py
@@ -10,11 +10,4 @@ def convertToString(n, base):
 print convertToString(n,16)
 
 
-def reverseString(s,i):
-	if i == len(s)-1:
-		return s[i]
-	else:
-		return reverseString(s,i+1) + s[i]
-
-print reverseString('abhay doke',0)
 
